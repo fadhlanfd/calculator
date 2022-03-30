2781c1ecddb5d53de8a9ee1624873fd69748976b
Fadhlan Fadhlillah <100657400+fadhlanfd@users.noreply.github.com>
2022-03-30 22:33:47 +0700

    Add Calculator website files

---
+++ script.js
@@ -0,0 +1,38 @@
+function clearOutput(){
+  document.getElementById("output").innerHTML="0";
+}
+
+function forclear() {
+      document.getElementById("output").innerHTML = "0";
+}
+
+function removeZero() {
+      var value = document.getElementById("output").innerHTML;
+      if (value == "0") {
+           value = " "
+           document.getElementById("output").innerHTML = value;
+      }
+}
+
+function perc() {
+      removeZero();
+      var value = document.getElementById("output").innerHTML;
+      value = value / 100;
+      document.getElementById("output").innerHTML = value;
+}
+
+function fordisplay(value) {
+      removeZero();
+      document.getElementById("output").innerHTML += value;
+}
+
+function solve() {
+     var equation = document.getElementById("output").innerHTML;
+     var solved = eval(equation);
+     document.getElementById('output').innerHTML = solved;
+}
+
+function deleteLastNumber(){
+    var test = document.getElementById("output").innerHTML;
+    document.getElementById("output").innerHTML = test.slice(0 , -1);
+}
